9ff82111b1b3d2aeeaea4ecfed1c1a41b257a0a4
aleksandrgrek <goldoner@gmail.com>
2021-11-23 13:27:28 +0100

    jar update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,12 +19,11 @@ public class Main {
         SerialPort comPort = SerialPort.getCommPort("/dev/ttyUSB0");
 
 
-        comPort.setBaudRate(600);
+        comPort.setBaudRate(3600);
         comPort.setParity(SerialPort.NO_PARITY);
         comPort.setNumDataBits(8);
         comPort.openPort();
         System.out.println("Is port opened: " + comPort.isOpen());
-         comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
         InputStream in = comPort.getInputStream();
         try {
             for (int j = 0; j < 1000; ++j)
