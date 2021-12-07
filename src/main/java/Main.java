9a4b5291bcf676ffc6baf1197b3cd46e1d359f95
aleksandrgrek <goldoner@gmail.com>
2021-12-07 14:04:43 +0100

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
         comPort.setBaudRate(9600);
         comPort.setNumDataBits(8);
         comPort.setParity(SerialPort.NO_PARITY);
-        comPort.setNumStopBits(SerialPort.TWO_STOP_BITS);
+        comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
        // comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
 
         // 9600 19200 38400
