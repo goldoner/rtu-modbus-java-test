8d2372b01aabf077300a5f9b6614c58275468630
aleksandrgrek <goldoner@gmail.com>
2022-01-05 13:09:10 +0300

    auto update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.fazecast.jSerialComm.SerialPort;
+import de.re.easymodbus.modbusclient.ModbusClient;
 
 import java.io.InputStream;
 import java.util.Arrays;
@@ -7,52 +8,37 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+//
+//        // /dev/ttyUSB0
+//
+//
+//        System.out.println(Arrays.toString(Arrays.stream(SerialPort.getCommPorts()).toArray()));
+//
 
-        // /dev/ttyUSB0
-
-
-        System.out.println(Arrays.toString(Arrays.stream(SerialPort.getCommPorts()).toArray()));
-
-
-       // SerialPort comPort = SerialPort.getCommPorts()[0];
+        // SerialPort comPort = SerialPort.getCommPorts()[0];
         SerialPort comPort = SerialPort.getCommPort("/dev/ttyUSB0");
-        comPort.setBaudRate(600);
-        comPort.setNumDataBits(8);
+        comPort.setBaudRate(9600);
         comPort.setParity(SerialPort.NO_PARITY);
-//        comPort.setNumStopBits(SerialPort.TWO_STOP_BITS);
         comPort.setNumStopBits(SerialPort.ONE_STOP_BIT);
-
-        // 9600 19200 38400
-
-
-        comPort.openPort();
-        System.out.println("Is port opened: " + comPort.isOpen());
-        try {
-            while (true)
-            {
-                while (comPort.bytesAvailable() == 0) {
-                    Thread.sleep(20);
-                }
-
-                byte[] readBuffer = new byte[comPort.bytesAvailable()];
-                int numRead = comPort.readBytes(readBuffer, readBuffer.length);
-                System.out.println("Read " + numRead + " bytes.");
-            }
-        } catch (Exception e) { e.printStackTrace(); }
-        comPort.closePort();
-
+//
+//        // 9600 19200 38400
+//
+//
 //        comPort.openPort();
 //        System.out.println("Is port opened: " + comPort.isOpen());
-//         comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-//        InputStream in = comPort.getInputStream();
 //        try {
-//            for (int j = 0; j < 1000; ++j)
-//                System.out.print((char) in.read());
-//            in.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+//            while (true)
+//            {
+//                while (comPort.bytesAvailable() == 0) {
+//                    Thread.sleep(20);
+//                }
+//
+//                byte[] readBuffer = new byte[comPort.bytesAvailable()];
+//                int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+//                System.out.println("Read " + numRead + " bytes.");
+//            }
+//        } catch (Exception e) { e.printStackTrace(); }
 //        comPort.closePort();
 
 //        SerialPort serialPort = new SerialPort("/dev/ttyUSB0", 600, Parity.NONE, 8);
@@ -61,11 +47,18 @@ public class Main {
 //        System.out.println();
 
 
-//        ModbusClient modbusClient = new ModbusClient();
-//        modbusClient.Connect("/dev/ttyUSB0");
-//        System.out.println("Is connected with default parameters : " + modbusClient.isConnected());
-//        System.out.println();
-//
+        ModbusClient modbusClient = new ModbusClient();
+        modbusClient.Connect("/dev/ttyUSB0");
+        System.out.println("Is connected with default parameters : " + modbusClient.isConnected());
+
+        System.out.println((float) modbusClient.ReadHoldingRegisters(4024, 1)[0]);
+
+
+
+        System.out.println((float) modbusClient.ReadHoldingRegisters(1000, 3)[0]);
+
+        System.out.println();
+
 //        modbusClient.Disconnect();
     }
 }
---
+++ src/main/java/push.sh
@@ -0,0 +1,4 @@
+mvn clean package
+git add .
+git commit -m "auto update"
+git push origin master
